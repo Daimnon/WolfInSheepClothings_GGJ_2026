42d29012a1b8806b37c2e7afbdb990c73df2c664
CreatureMind <umarovart@gmail.com>
2026-01-31 16:52:29 +0200

    started particles

---
+++ Assets/Scripts/ParticlesScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ParticlesScript : MonoBehaviour
+{
+    public GameObject[] particles;
+    public GameObject[] sheep;
+    public GameObject[] hay;
+    public Transform legs;
+    public Transform Gun;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
