ab6bf8610151024b3c9043ca3690c1f9b9c530be
Daimnon <49341329+Daimnon@users.noreply.github.com>
2026-01-31 16:37:29 +0200

    Day NightUI Works

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class DayNightCycle : MonoBehaviour
 {
     [SerializeField] private Light directionalLight;
+    [SerializeField] private GameObject dayNightImg;
     [SerializeField] private float maxRotationX = 180f;
     [SerializeField] private Vector3 startRotation = new(50.0f, -30.0f, 0.0f);
+    [SerializeField] private Vector3 startImgRotation = new(0.0f, 0.0f, -30.0f);
     private Coroutine _rotationCoroutine;
 
     public void StartDayNightCycleRotation()
@@ -19,11 +21,14 @@ public class DayNightCycle : MonoBehaviour
         {
             float progress = 1f - (GameManager.Instance.CurrentTimeLeft / GameManager.Instance.TimerDuration);
             float xRotation = Mathf.Lerp(startRotation.x, maxRotationX, progress);
+            float zImgRotation = Mathf.Lerp(startImgRotation.x, maxRotationX, progress);
             directionalLight.transform.rotation = Quaternion.Euler(xRotation, -30.0f, 0.0f);
+            dayNightImg.transform.rotation = Quaternion.Euler(0.0f, 0.0f, -zImgRotation);
             yield return null;
         }
 
         // Ensure exact final rotation
         directionalLight.transform.rotation = Quaternion.Euler(maxRotationX, -30.0f, 0.0f);
+        dayNightImg.transform.rotation = Quaternion.Euler(0.0f, 0.0f, -maxRotationX);
     }
 }
